ebb606fb8400e68c65f29c2acddbd08485e4a0b2
bubblestew95 <bubblestew95@gmail.com>
2024-11-14 11:43:19 +0900

    다트 날라가는 기능 구현

---
+++ Assets/Scripts/Dart.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class Dart : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private float dartBoardZPos = 10f;
+
+    private Vector3 dir = Vector3.zero;
+
+    // 다트 판까지의 거리 측정을 위한 디버깅 스피어 그리기
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(new Vector3(0f, 0f, dartBoardZPos), 2f);
+    }
+
+    // startPos ~ endPos 로 일직선 발사 시작
+    // 추후에 포물선 발사 구현 가능?
+    public void ThrowDart(Vector3 _startPos, Vector3 _endPos)
+    {
+        dir = (_endPos - _startPos).normalized;
+
+        StartCoroutine(DartFlyingCoroutine());
+    }
+
+    // 다트 발사 중 이동 코루틴
+    private IEnumerator DartFlyingCoroutine()
+    {
+        while(transform.position.z < dartBoardZPos)
+        {
+            transform.Translate(dir * speed * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+}
