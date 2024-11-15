60f61cd3493a60e0a54b24044f08f9f2178372cd
Jin <voltagevault@gmail.com>
2024-11-15 09:31:35 +0900

    포인터 UI 완성(1차)

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -1,40 +1,46 @@
-using System.Collections;
 using UnityEngine;
 
 public class Pointer : MonoBehaviour
 {
-    [SerializeField]
     private RectTransform rectTr = null;
 
-    private Vector2 resetPos = new Vector2(-10f, -10f);
+    private Vector3 cursorPos;   // 마우스 커서 위치
+    private Vector3 pointerPos;  // 랜덤 포인터 위치
+    private Vector3 resetPos; // 커서가 오브젝트 벗어날 시 마우스 리셋 위치
+
+    public float pointerRndRng = 80f;  // 흔들림 범위
+    public float moveSpeed = 8f;      // 이동 속도 조절
 
 
     private void Start()
     {
-        InvokeRepeating("DrawPointerCoroutine", 0f, 0.1f);
+        pointerPos = resetPos;  // 포인터 위치 초기화
+        rectTr = GetComponent<RectTransform>();
     }
-
 
     private void Update()
     {
-
+        DrawPointer();
     }
 
-    private void DrawPointerCoroutine()
+    private void DrawPointer()
     {
-        Vector3 mousePos = Input.mousePosition;
-
-        Vector3 mouseScreenToWorld =
-            Camera.main.ScreenToWorldPoint(mousePos);
-        Ray MousePosToRay =
-            Camera.main.ScreenPointToRay(mousePos);
+        resetPos = new Vector3(-10f, -10f, 0f);
+        cursorPos = Input.mousePosition;
+        Ray cursorPosToRay =
+            Camera.main.ScreenPointToRay(cursorPos);
         RaycastHit hit;
 
-        if (Physics.Raycast(MousePosToRay, out hit))
+
+        if (Physics.Raycast(cursorPosToRay, out hit))
         {
-            rectTr.position = new Vector2(
-                mousePos.x + Random.Range(-10f, 10f),
-                mousePos.y + Random.Range(-10f, 10f));
+            pointerPos = cursorPos + new Vector3(
+                Random.Range(-pointerRndRng, pointerRndRng),
+                Random.Range(-pointerRndRng, pointerRndRng),
+                0);
+
+            // Lerp를 이용하여 포인터에서 포인터 사이 움직임 보간
+            rectTr.position = Vector3.Lerp(rectTr.position, pointerPos, Time.deltaTime * moveSpeed);
         }
         else
         {
